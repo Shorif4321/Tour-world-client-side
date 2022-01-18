f3e12f5d93a3d0043b387163f2c930f996ee3e69
Shorif4321 <shorifali4321@gmail.com>
2022-01-18 23:47:51 +0600

    finished

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -4,7 +4,7 @@ import "./About.css";
 const About = () => {
     return (
         <div className="about-banner">
-            <h1>About Us</h1>
+            <h1>About Us</h1> 
         </div>
     );
 };
